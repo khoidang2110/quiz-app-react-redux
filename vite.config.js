1746cde6ba228828743efcb3e7e1acf10feff59d
khoidang2110 <khoidang2110@gmail.com>
2023-09-07 01:30:46 +0700

    i

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [
-        {
-            find: /^jss-plugin-(.*)$/,
-            replacement: "$1",
-            customResolver: (id) => {
-                if (id === "{}") {
-                    id = "global";
-                }
-                return resolve(__dirname, `./node_modules/jss-plugin-${id}/src/index.js`);
-            },
-        },
-    ],
-},
+  
 })
